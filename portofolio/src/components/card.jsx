4ce1bf0e04d3eed66d50d6c426a0df9b73e0ac46
Christ-edmund23 <94790146+Christ-edmund23@users.noreply.github.com>
2025-03-12 15:20:27 +0700

    test 4

--- portofolio/src/components/card.jsx
+++ portofolio/src/components/card.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const card = (props) => {
   return (
     <div className='flex flex-row mb-10'>
-                <div className='w-[30%] text-gray-400 text-wrap'>
+                <div className='w-[20%] text-gray-400 text-wrap'>
                   {props.duration}
                 </div>
-                <div className='w-[70%] ml-2'>
+                <div className='w-[80%] ml-3'>
                   <p className='text-lg font-bold'>{props.title}</p>
                   <p className='text-gray-400 text-sm mb-5'>{props.company}</p>
                   <p className='text-balance'>
